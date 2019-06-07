0ba3524d6408407e0092b90c677f3b12c2c54aed
John <johnjohnx4@gmail.com>
2019-06-07 03:53:56 -0400

    misc changes

--- src/pages/Notes/index.js
+++ src/pages/Notes/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import NavBar from '../../components/NavBar';
+import { Grid, Paper } from '@material-ui/core';
+import NotesRender from './NotesRender';
+import './index.scss';
 
 class LoginPage extends Component {
   render() {
     return (
       <div className='notes'>
         <NavBar history={this.props.history} />
-        This is the Notes Page
+        <div className='container'>
+          <Grid container spacing={10}>
+            <Paper>
+              <NotesRender />
+            </Paper>
+          </Grid>
+        </div>
       </div>
     );
   }
