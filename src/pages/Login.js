80beb1e36f4bfdf7c8ec63091730548c862fd1c2
John Correia <johnjohnx4@gmail.com>
2021-02-14 21:21:15 -0500

    update login page to material-ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  CardTitle,
-  CardBody,
-  CardLink,
-  Button,
-  Form,
-  FormGroup,
-  Input,
-} from 'reactstrap';
-
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { register, login, ping } from '../actions';
+import NoterBannerImg from '../assets/images/noterbanner.png';
 
-const LoginPage = (props) => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  registerLink: {
+    cursor: 'pointer',
+  },
+}));
+
+export default function LoginPage(props) {
   const [registerState, setRegisterState] = useState(false);
   const [emailString, setEmailString] = useState('');
   const [passwordString, setPasswordString] = useState('');
@@ -31,86 +49,83 @@ const LoginPage = (props) => {
   useEffect(() => {
     ping();
   }, []);
+  const classes = useStyles();
 
   return (
-    <Container className='mt-5'>
-      <Row className='mt-5'>
-        <Col>
-          <Card className='px-5 mt-5'>
-            <CardBody>
-              <CardTitle>Welcome!</CardTitle>
-              <Form>
-                <FormGroup>
-                  <Input
-                    type='email'
-                    name='email'
-                    id='email'
-                    value={emailString}
-                    onChange={(e) => setEmailString(e.target.value)}
-                    placeholder='Email'
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Input
-                    type='password'
-                    name='password'
-                    id='password'
-                    value={passwordString}
-                    onChange={(e) => setPasswordString(e.target.value)}
-                    placeholder='Password'
-                  />
-                </FormGroup>
-                {registerState && (
-                  <FormGroup>
-                    <Input
-                      type='password'
-                      name='confirm'
-                      id='confirm'
-                      value={confirmString}
-                      onChange={(e) => setConfirmString(e.target.value)}
-                      placeholder='Confirm Password'
-                    />
-                  </FormGroup>
-                )}
-                <FormGroup>
-                  <Button onClick={handleSubmit}>
-                    {registerState ? 'Register' : 'Log In'}
-                  </Button>
-                  <Button
-                    className='ml-3'
-                    onClick={() => login('test', 'test', props.history)}
-                  >
-                    Example User
-                  </Button>
-                </FormGroup>
-                {registerState ? (
-                  <FormGroup>
-                    Already have an account?{' '}
-                    <CardLink
-                      style={{ cursor: 'pointer' }}
-                      onClick={toggleRegisterState}
-                    >
-                      Login
-                    </CardLink>
-                  </FormGroup>
-                ) : (
-                  <FormGroup>
-                    Dont have an account?{' '}
-                    <CardLink
-                      style={{ cursor: 'pointer' }}
-                      onClick={toggleRegisterState}
-                    >
-                      Register
-                    </CardLink>
-                  </FormGroup>
-                )}
-              </Form>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <img src={NoterBannerImg} />
+        <Typography component='h1' variant='h5'>
+          {registerState ? 'Register' : 'Sign in'}
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+            value={emailString}
+            onChange={(e) => setEmailString(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+            value={passwordString}
+            onChange={(e) => setPasswordString(e.target.value)}
+          />
+          {registerState && (
+            <TextField
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              name='confirm'
+              label='Confirm Password'
+              type='password'
+              id='confirm'
+              autoComplete='current-password'
+              value={confirmString}
+              onChange={(e) => setConfirmString(e.target.value)}
+            />
+          )}
+
+          <Button
+            fullWidth
+            variant='contained'
+            color='primary'
+            onClick={handleSubmit}
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link
+                className={classes.registerLink}
+                onClick={toggleRegisterState}
+                variant='body2'
+              >
+                {registerState
+                  ? 'Already have an account? Log in'
+                  : "Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
     </Container>
   );
-};
-
-export default LoginPage;
+}
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,91 +1,244 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  CardTitle,
-  CardText,
-  CardBody,
-  CardDeck,
-  Button,
-  Form,
-  FormGroup,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Input,
-} from 'reactstrap';
+// import React, { useState, useEffect } from 'react';
+// import {
+//   Container,
+//   Row,
+//   Col,
+//   Card,
+//   CardTitle,
+//   CardText,
+//   CardBody,
+//   CardDeck,
+//   Button,
+//   Form,
+//   FormGroup,
+//   Modal,
+//   ModalHeader,
+//   ModalBody,
+//   ModalFooter,
+//   Input,
+// } from 'reactstrap';
 
-import { fetchNotes, createNote } from '../actions';
+// import { fetchNotes, createNote } from '../actions';
 
-const AddNote = (props) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+// const AddNote = (props) => {
+//   const [title, setTitle] = useState('');
+//   const [content, setContent] = useState('');
 
-  const handleCreateNote = () => {
-    createNote(title, content);
-    props.toggle();
-  }
+//   const handleCreateNote = () => {
+//     createNote(title, content);
+//     props.toggle();
+//   }
+
+//   return (
+//     <Form className='m-4'>
+//       <FormGroup>
+//         <Input className='mb-3'
+//           value={title}
+//           onChange={(e) => setTitle(e.target.value)}
+//           placeholder='Title'
+//         />
+//         <Input className='mb-3'
+//           value={content}
+//           onChange={(e) => setContent(e.target.value)}
+//           placeholder='Content'
+//         />
+//         <Button color='primary' onClick={handleCreateNote}>
+//           Submit
+//         </Button>
+//       </FormGroup>
+//     </Form>
+//   );
+// };
+
+// const NotesPage = (props) => {
+//   const [notes, setNotes] = useState([]);
+//   const [modal, setModal] = useState(false);
+
+//   const toggle = () => setModal(!modal);
+
+//   useEffect(() => {
+//     fetchNotes().then((res) => setNotes(res.data));
+//   }, [modal]);
+
+//   const handleNewNote = () => {
+//     toggle();
+//     console.log('activated');
+//   };
+
+//   return (
+//     <Container className='mt-5'>
+//       <Modal className='p-5' isOpen={modal} toggle={toggle}>
+//         <ModalHeader>Add New Note</ModalHeader>
+//         <AddNote toggle={toggle} />
+//       </Modal>
+//       <CardDeck className='m-auto'>
+//         {notes.map((note) => (
+//           <Card key={note._id} className='mb-3' style={{ minWidth: '315px' }}>
+//             <CardBody>
+//               <CardTitle tag='h5'>{note.title}</CardTitle>
+//               <CardText>{note.content}</CardText>
+//             </CardBody>
+//           </Card>
+//         ))}
+//       </CardDeck>
+//       <Button onClick={handleNewNote} className='ml-3' color='primary'>
+//         Add New Note
+//       </Button>
+//     </Container>
+//   );
+// };
+
+// export default NotesPage;
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
 
   return (
-    <Form className='m-4'>
-      <FormGroup>
-        <Input className='mb-3'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder='Title'
-        />
-        <Input className='mb-3'
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          placeholder='Content'
-        />
-        <Button color='primary' onClick={handleCreateNote}>
-          Submit
-        </Button>
-      </FormGroup>
-    </Form>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Album layout
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Something short and leading about the collection below—its contents, the creator, etc.
+              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              entirely.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Main call to action
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
   );
-};
-
-const NotesPage = (props) => {
-  const [notes, setNotes] = useState([]);
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
-
-  useEffect(() => {
-    fetchNotes().then((res) => setNotes(res.data));
-  }, [modal]);
-
-  const handleNewNote = () => {
-    toggle();
-    console.log('activated');
-  };
-
-  return (
-    <Container className='mt-5'>
-      <Modal className='p-5' isOpen={modal} toggle={toggle}>
-        <ModalHeader>Add New Note</ModalHeader>
-        <AddNote toggle={toggle} />
-      </Modal>
-      <CardDeck className='m-auto'>
-        {notes.map((note) => (
-          <Card key={note._id} className='mb-3' style={{ minWidth: '315px' }}>
-            <CardBody>
-              <CardTitle tag='h5'>{note.title}</CardTitle>
-              <CardText>{note.content}</CardText>
-            </CardBody>
-          </Card>
-        ))}
-      </CardDeck>
-      <Button onClick={handleNewNote} className='ml-3' color='primary'>
-        Add New Note
-      </Button>
-    </Container>
-  );
-};
-
-export default NotesPage;
+}
